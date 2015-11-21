b9b7d1c6ed926bef9d134f2431b0144a5f73b2d8
Thomas LE ROUX <github@november-eleven.fr>
2015-11-21 02:31:08 +0100

    feat(configuration): Add validator and linter handler

---
+++ configuration/validator.go
@@ -0,0 +1,46 @@
+package configuration
+
+import (
+	"errors"
+)
+
+var (
+	// ErrOutputRequired is returned when output isn't defined in the configuration.
+	ErrOutputRequired = errors.New("configuration(output): a path is required")
+	// ErrPathRequired is returned when path isn't defined in the configuration.
+	ErrPathRequired = errors.New("configuration(install): path is required")
+	// ErrImageRequired is returned when path isn't defined in the configuration.
+	ErrImageRequired = errors.New("configuration(install): image is required")
+	// ErrNameRequired is returned when image name isn't defined in the configuration.
+	ErrNameRequired = errors.New("configuration(compose): name is required")
+	// ErrConfigurationEmpty is returned when Install is called with an empty configuration.
+	ErrConfigurationEmpty = errors.New("configuration is required")
+)
+
+// Validate return an error if the given Configuration has flaw.
+func Validate(c *Configuration) error {
+
+	if c == nil {
+		return ErrConfigurationEmpty
+	}
+
+	if c.Output == "" {
+		return ErrOutputRequired
+	}
+
+	if c.Install.Path == "" {
+		return ErrPathRequired
+	}
+
+	if c.Install.Image == "" {
+		return ErrImageRequired
+	}
+
+	if c.Compose.Name == "" {
+		return ErrNameRequired
+	}
+
+	Values(c)
+
+	return nil
+}
