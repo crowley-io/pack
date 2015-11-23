d5b935577d8fbc034efcb3e22c7ce061c4b8efc1
Thomas LE ROUX <github@november-eleven.fr>
2015-11-23 11:50:12 +0100

    feat(compose): Add compose handler

---
+++ compose/compose.go
@@ -0,0 +1,36 @@
+package compose
+
+import (
+	//	"errors"
+	"os"
+
+	"github.com/crowley-io/pack/configuration"
+	"github.com/crowley-io/pack/docker"
+)
+
+// Compose create a new image.
+func Compose(client docker.Docker, configuration *configuration.Configuration) error {
+
+	if err := configuration.Validate(); err != nil {
+		return err
+	}
+
+	name := configuration.Compose.Name
+	noCache := configuration.Compose.NoCache
+	pull := configuration.Compose.Pull
+	directory, err := os.Getwd()
+
+	if err != nil {
+		return err
+	}
+
+	option := docker.BuildOptions{
+		Name:      name,
+		Directory: directory,
+		Pull:      pull,
+		NoCache:   noCache,
+	}
+
+	return client.Build(option)
+
+}
