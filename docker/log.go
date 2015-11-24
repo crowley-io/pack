74809154023e28524badc2989ebb8975d88b97f9
Thomas LE ROUX <github@november-eleven.fr>
2015-11-24 11:41:38 +0100

    feat(docker): Add default LogStream

--- docker/log.go
+++ docker/log.go
@@ -2,6 +2,7 @@ package docker
 
 import (
 	"io"
+	"os"
 
 	api "github.com/fsouza/go-dockerclient"
 )
@@ -15,6 +16,14 @@ type LogStream struct {
 // See Docker interface
 func (d docker) Logs(id string, stream LogStream) error {
 	return d.client.Logs(logsOptions(id, stream))
+}
+
+// NewLogStream return a default LogStream using OS stdout and stderr.
+func NewLogStream() LogStream {
+	return LogStream{
+		Out: os.Stdout,
+		Err: os.Stderr,
+	}
 }
 
 func logsOptions(container string, stream LogStream) api.LogsOptions {
