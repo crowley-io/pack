bf06b537760aa82162b6f19b57fba60e9e377c35
Thomas LE ROUX <github@november-eleven.fr>
2015-11-24 14:41:36 +0100

    refactor(docker): Refactor push.

--- docker/push.go
+++ docker/push.go
@@ -6,29 +6,39 @@ import (
 
 const (
 	pushRawJSONStream = false
+	pushRegistry      = ""
 )
 
 // PushOptions contains the push configuration for the docker daemon.
 type PushOptions struct {
-	Name     string
-	Tag      string
-	Registry string
-	Command  string
-	Env      []string
-	Volumes  []string
+	Name       string
+	Repository string
+	Registry   string
+	Tag        string
 }
 
 // See Docker interface
 func (d docker) Push(option PushOptions, stream LogStream) error {
+
+	o := TagOptions{
+		Name:       option.Name,
+		Repository: option.Repository,
+		Tag:        option.Tag,
+	}
+
+	if err := d.Tag(o); err != nil {
+		return err
+	}
+
 	return d.client.PushImage(pushImageOptions(option, stream), pushAuthConfiguration(option))
 }
 
 func pushImageOptions(option PushOptions, stream LogStream) api.PushImageOptions {
 	return api.PushImageOptions{
-		Name:          option.Name,
+		Name:          option.Repository,
 		Tag:           option.Tag,
-		Registry:      option.Registry,
 		OutputStream:  stream.Out,
+		Registry:      pushRegistry,
 		RawJSONStream: pushRawJSONStream,
 	}
 }
